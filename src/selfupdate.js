cbf819566e1421e06adca014fee51e1694caa815
George Czabania <george@mish.guru>
2019-03-23 02:22:53 +1300

    Update check interval after successfully installing

--- src/selfupdate.js
+++ src/selfupdate.js
@@ -15,7 +15,8 @@ const DEFAULT_CHECK_INTERVAL = 1 * 60 * 60 * 1000
 // You can block a thread from running by making it await this promise
 const BLOCK_THREAD = new Promise(() => false)
 
-const updateCheckInterval = (key) => {
+const updateCheckInterval = (pkgName) => {
+  const key = `last-check:${pkgName}`
   const value = Number(Date.now()).toString()
   return cacache.put(CACHE, key, value)
 }
@@ -23,19 +24,14 @@ const updateCheckInterval = (key) => {
 const isBeyondCheckInterval = async (pkgName, checkInterval) => {
   const key = `last-check:${pkgName}`
 
-  let isBeyond = true
   try {
     const cacheObj = await cacache.get(CACHE, key)
     const time = Number(cacheObj.data.toString('utf8'))
-    isBeyond = (time + checkInterval) < Date.now()
+    const isBeyond = (time + checkInterval) < Date.now()
+    return isBeyond
   } catch (notFound) {
+    return true
   }
-
-  if (isBeyond) {
-    await updateCheckInterval(key)
-  }
-
-  return isBeyond
 }
 
 
@@ -43,7 +39,6 @@ const selfupdate = async (pkg, options = {}) => {
   const { checkInterval = DEFAULT_CHECK_INTERVAL } = options
 
   const shouldCheck = await isBeyondCheckInterval(pkg.name, checkInterval)
-
   if (shouldCheck === false) {
     return
   }
@@ -53,6 +48,7 @@ const selfupdate = async (pkg, options = {}) => {
 
   const newVersionAvailable = cmp(pkg.version, latestVersion) < 0
   if (newVersionAvailable === false) {
+    await updateCheckInterval(pkg.name)
     spinner.stop()
     return
   }
@@ -67,10 +63,11 @@ const selfupdate = async (pkg, options = {}) => {
     await delay(3 * 1000)
     await respawnProcess()
     spinner.succeed(`You are now using the latest version :)`)
+    await updateCheckInterval(pkg.name)
     return BLOCK_THREAD
   } catch (error) {
     spinner.fail(`Error installing ${pkg.name} v${latestVersion}`)
-    console.error({ error })
+    throw error
   }
 }
 
